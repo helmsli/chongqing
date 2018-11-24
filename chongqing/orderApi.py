4870c28c1fd4e7cfa9359fa2774918b3f3a8637a
guoqiang <liguoqiang@bj.xinwei.com.cn>
2018-11-24 18:07:53 +0800

    add order api

--- chongqing/orderApi.py
+++ chongqing/orderApi.py
@@ -10,6 +10,21 @@ import codecs
 orderIdServerUrl=""
 #order接入的服务地址，需要包含工程名，不能以/结尾
 orderAccessServerUrl=""
+class ProcessResult(object):
+    def __init__(self,retCode,retMsg):
+        return {'retCode':retCode,'retMsg':retMsg}
+
+    def __init__(self):
+        return {'retCode':0, 'retMsg': ""}
+    pass
+
+
+def getDbid(orderId)
+    if not orderId.strip() and len(orderId) >= 7:
+        return orderId[-4:]
+    return ""
+def getSuccessResponse()
+    processResult =
 #定义创建的订单的上下文
 class OrderMainContext(object):
     #初始化map为空
@@ -89,41 +104,51 @@ def getOrderId(category,ownerKey):
         print('\n')
     return ""
 
-def getOrderId(category,ownerKey):
-    #构造服务器地址
-    serverUrl = '%s/%s/%s/createOrderId' % (orderIdServerUrl, category,ownerKey)
-    #构造请求参数
-    payload = {'requestTransId': '', 'requestTime': '', 'token': '', 'jsonString': ''}
-    r = requests.post(url=serverUrl, json=payload)  # 带参数的GET请求
-    r.encoding = 'utf-8'  # 这里添加一行
-    response = json_loads_byteified(r.text)
-    if response['retCode'] == 0:
-        return(response['responseInfo'])
-    else:
-        print("get orderId Error:")
-        print(response)
-        print('\n')
-    return ""
-
+'''
+创建订单
+'''
 def createOrder(orderMainContext):
     category = orderMainContext.getCategory()
     ownerKey = orderMainContext.getOwnerKey()
     dbId = orderMainContext.getDbid()
-
+    orderId = orderMainContext.getOrderId()
     #构造服务器地址 /{category}/{dbId}/{orderId}/createOrder
-    serverUrl = '%s/%s/%s/createOrderId' % (orderIdServerUrl, category,ownerKey)
+    serverUrl = '%s/%s/%s/%s/createOrder' % (orderAccessServerUrl,category,dbId,orderId)
     #构造请求参数
-    payload = {'requestTransId': '', 'requestTime': '', 'token': '', 'jsonString': ''}
+    payload = orderMainContext
     r = requests.post(url=serverUrl, json=payload)  # 带参数的GET请求
     r.encoding = 'utf-8'  # 这里添加一行
     response = json_loads_byteified(r.text)
-    if response['retCode'] == 0:
-        return(response['responseInfo'])
-    else:
-        print("get orderId Error:")
-        print(response)
-        print('\n')
-    return ""
+    return response
+'''
+启动订单
+'''
+def startOrder(category,orderId):
+    dbId = getDbid(orderId)
+    if dbId.strip():
+        processResult = ProcessResult(-1,'dbid is null')
+        return processResult
+    #构造服务器地址 /{category}/{dbId}/{orderId}/startOrder
+    serverUrl = '%s/%s/%s/%s/startOrder' % (orderAccessServerUrl,category,dbId,orderId)
+    r = requests.get(url=serverUrl)  # 带参数的GET请求
+    r.encoding = 'utf-8'  # 这里添加一行
+    response = json_loads_byteified(r.text)
+    return response
+
+'''
+获取订单系统中的上下文
+'''
+def getOrderContext(category,orderId,keyList):
+    dbId = getDbid(orderId)
+
+    #构造服务器地址 /{category}/{dbId}/{orderId}/getContextData
+    serverUrl = '%s/%s/%s/%s/getContextData' % (orderAccessServerUrl,category,dbId,orderId)
+    #构造请求参数
+    payload = keyList
+    r = requests.post(url=serverUrl, json=payload)  # 带参数的GET请求
+    r.encoding = 'utf-8'  # 这里添加一行
+    response = json_loads_byteified(r.text)
+    return response
 
 '''
 query the course Information 
