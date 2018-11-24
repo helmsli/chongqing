0fe0ad7c66aa5063183a2c438734473ffa2d19f4
guoqiang <liguoqiang@bj.xinwei.com.cn>
2018-11-24 11:36:25 +0800

    release no PipLine

--- chongqing/items.py
+++ chongqing/items.py
@@ -18,6 +18,7 @@ class ChongqingItem(scrapy.Item):
     #原始的页面
     lastThreeResult = scrapy.Field()
     pass
+
 #开奖号码对象模型
 class WinNumberItem(scrapy.Item):
 
@@ -26,15 +27,9 @@ class WinNumberItem(scrapy.Item):
     # 开奖时间
     openDate = scrapy.Field()
     # 开奖号码
-    firstNumber = scrapy.Field()
-    # 期号
-    secondNumber = scrapy.Field()
-    # 开奖时间
-    thirdNumber = scrapy.Field()
-    #原始的页面
-    fourthNumber = scrapy.Field()
+    winNumbers = scrapy.Field()
 
-    fifthNumber = scrapy.Field()
+
     pass
 
 #个位开奖模型
--- chongqing/spiders/chongqing_spider.py
+++ chongqing/spiders/chongqing_spider.py
@@ -1,24 +1,58 @@
 # encoding=utf8
 import scrapy
 from chongqing.items import  ChongqingItem
+from chongqing.items import  WinNumberItem
+from lxml import etree
 
 class ChongqingSpider(scrapy.Spider):
     name = "chongqing"
     allowed_domains = ["caipiao.163.com"]
     start_urls = [
-        "http://caipiao.163.com/award/cqssc/"
+        #"http://caipiao.163.com/award/cqssc/",
+        "http://caipiao.163.com/award/cqssc/20181122.html"
+
     ]
 
-
+    #读取开奖表格
     def readTable(self,response):
-        chongqingTables = response.xpath(
-            '//div[@class="lottery-results"]/table[1]')
-        return chongqingTables
+        chongqingTables = response.xpath('//div[@class="lottery-results"]/table[1]')
+        return chongqingTables;
+    #读取表格的所有开奖记录
+    def getWinNumberTd(self,table):
+        openResultTd = table.xpath('//tr/td[@data-win-number and @data-period]').extract()
+        return openResultTd;
+    def getWinNumber(self,winNumberTd):
+        #对某一个TR下的TD进行循环
+        winTrIndexList=[]
+        for winnumberOneTd in winNumberTd:
+            winNumberItem = WinNumberItem()
+            #将文本winnumberOneTd转换为HTML
+            html = etree.fromstring(winnumberOneTd)
+            #获取开奖号码,结果为列表
+            winNumber = html.xpath(
+                '//@data-win-number')
+            #print('winnumber:' + '\n')
+            #print(type(winNumber))
+            #如果列表个数大于0
+            if len(winNumber)>0:
+                #获取开奖的号码
+                winNumbers = winNumber[0]
+                winNumberItem['winNumbers']=winNumbers
+            #获取开奖期号，结果为列表
+            dataPeriod = html.xpath(
+                '//@data-period')
+            if len(dataPeriod)>0:
+                #获取开奖的期号
+                winNumberItem['periodNo'] = dataPeriod[0]
+            winTrIndexList.append(winNumberItem)
+        #print(winTrIndexList)
+        #print('after get winnumber tr:')
+        return winTrIndexList
 
-    def readWinNumber(self,table,index):
-        chongqingTableAlltr = table.xpath(
-            '//tr[2]/td[@class="award-winNum"]/text()').extract()
-        print(chongqingTableAlltr)
+        winNumber = winNumberTd.xpath(
+            '//@data-win-number')
+        #print(winNumber)
+        print('winnumber')
 
 
     def readItemFromResponse(self,response):
@@ -26,50 +60,21 @@ class ChongqingSpider(scrapy.Spider):
 
 
     def parse(self, response):
-        //定义文件名称，保存原始的页面记录
+        chongqingItem =ChongqingItem()
+        #定义文件名称，保存原始的页面记录
         filename = 'chongqing.txt'
         with open(filename, 'wb') as f:
             f.write(response.body)
         print "*********************"
-        # 抓取页面中的表格对象
-        chongqingTables = response.xpath(
-            '//div[@class="lottery-results"]/table[1]')
+        # 抓取页面中的表格对象,得到一个表格的HTML
+        chongqingTables = self.readTable(response)
+        #获取表格中的所有开奖记录
+        getWinNumberTd = self.getWinNumberTd(chongqingTables)
+        winNumberList =self.getWinNumber(getWinNumberTd)
+        winNumberList.sort(key=lambda winNumberTemp: winNumberTemp['periodNo'], reverse=False)
 
-        for i in range(2,3):
-            print("********************* " +str(i))
-            chongqingTableChild = chongqingTables.xpath(
-            '//tr[2]/td[@class="start"]').extract()
-            print(chongqingTableChild)
-            print('55555555555555')
-            chongqingTableAlltr = response.xpath('//div[@class="lottery-results"]/table[1]/tr[2]/td[@class="start"]').extract()
-            chongqingTableTensPlace = response.xpath(
-                '//div[@class="lottery-results"]/table[1]/tr[2]/td[3]/text()').extract()
-            chongqingTableOnesPlace = response.xpath(
-                '//div[@class="lottery-results"]/table[1]/tr[2]/td[4]/text()').extract()
-            chongqingTableLastThree = response.xpath(
-                '//div[@class="lottery-results"]/table[1]/tr[2]/td[5]/text()').extract()
-
-            print(chongqingTableAlltr)
-            print(chongqingTableTensPlace)
-            print(chongqingTableOnesPlace)
-            print(chongqingTableLastThree)
-
-            print("############# " + str(i))
-
-
-            #for chongqingTableTr in chongqingTableAlltr:
-                #print(type(chongqingTableTr))
-                #print(chongqingTableTr)
-
-        #获取重庆时时彩的信息
-        #chongqingTableAlltr = response.xpath('//div[@class="lottery-results"]/table/tr/*').extract()
-        print(chongqingTableAlltr)
-        with open("result.txt",'wb') as f:
-
-            for chongqingTableTr in chongqingTableAlltr:
-
-                print(type(chongqingTableTr))
-                print(chongqingTableTr)
-
-                f.write(chongqingTableTr)
-                f.write(" "+'\n')
+        chongqingItem['winNumberResult']=winNumberList
+        #print(chongqingItem['winNumberResult'])
+        yield chongqingItem
+        #print(chongqingTables)
+        #print(self.readPeriod(chongqingTables,1))
